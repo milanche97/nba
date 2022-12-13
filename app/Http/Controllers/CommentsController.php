e5dbacd94806b97fcaddc55eb5b04d15bec1377e
milanche97 <milanrbajic@gmail.com>
2022-12-13 13:27:39 +0100

    baca mi neku gresku zadatak 8

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\CommentReceived;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 use App\Models\Team;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class CommentsController extends Controller
 {
@@ -25,12 +27,14 @@ class CommentsController extends Controller
         );
 
         $team = Team::find($team_id);
-
+        Mail::to($team->user)->send(new CommentReceived($team));
+        
         Comment::create([
             'content' => $request->get('content'),
             'team_id' => $team->id,
             'user_id' => Auth::user()->id,
         ]);
+
 
         return redirect()->route('single-team', ['id' => $team_id]);
 
--- resources/views/teams/forbidden-comment.blade.php
+++ resources/views/teams/forbidden-comment.blade.php
@@ -1,7 +1,2 @@
-@section('header-desc')
-    You used forbidden words in your comments!
-@endsection
-
-
     <h3>Ne budi bezobrazan!</h3>
 
